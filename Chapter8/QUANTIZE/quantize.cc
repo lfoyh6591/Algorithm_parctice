6d768476cff9a8f07b85b8bda68584bd3e9a13df
lfoyh6591 <lfoyh6591@naver.com>
2021-07-14 13:31:01 +0900

    problem link

--- Chapter8/QUANTIZE/quantize.cc
+++ Chapter8/QUANTIZE/quantize.cc
@@ -56,39 +56,15 @@ int quantize(int begin, int num){
     if(memo[begin][num-1] != -1){
         return memo[begin][num-1];
     }
-    /*if(num == 1){
-        memo[begin][0] = quantizeone(begin, n-1);
-        return memo[begin][0];
-    }*/
     int ret = 987654321;
     for(int i=1; begin+i<=n; i++){
         ret  = min(ret, quantizeone(begin, begin+i-1)+quantize(begin+i, num-1));
     }
-    /*for(int i=begin+1; i<n-num+1; i++){
-        ret = min(ret, quantizeone(begin, i-1) + quantize(i, num-1));
-    }*/
     memo[begin][num-1] = ret;
     //cout << "begin " << begin << " num " << num << " ret " << ret << " \n"; 
     return ret;
 }
 
-/*int quantizeone(int begin, int end){
-    if(memo2[begin][end]!=-1){
-        return memo2[begin][end];
-    }
-    int ret = 987654321;
-    int retv = 0;
-    int m = mean(begin, end);
-    
-    for(int j=begin; j<end; j++){
-        retv += (m-input.at(j))*(m-input.at(j));
-    }
-    ret = min(ret, retv);
-    retv = 0;
-
-    memo2[begin][end] = ret;
-    return ret;
-}*/
 
 int quantizeone(int begin, int end){
     int sum = psum[end] - (begin == 0 ? 0 : psum[begin-1]);
