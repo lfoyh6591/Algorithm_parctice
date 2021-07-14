90de76f7e65a9d946205851162f5d492d90e6796
lfoyh6591 <lfoyh6591@naver.com>
2021-07-14 13:45:50 +0900

    modifying tiling2.cc

---
+++ Chapter8/ASYMYILING/asymyling.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+long long memo[100];
+int n;
+int tiling(int);
+int main(){
+    int C;
+    cin >> C;    
+    for(int c=0; c<C; c++){
+        memset(memo, -1, sizeof(memo));
+        cin >> n;
+        int result = 0;
+        if(n>2){
+            if(n%2 == 0){
+                result = tiling(n) - tiling((n-2)/2) - tiling(n/2);
+            }
+            else{
+                result = tiling(n) - tiling(n/2);
+            }
+        }
+        result %=1000000007;
+        cout << (result<0 ? result+1000000007 : result)  << "\n";
+    }
+}
+
+int tiling(int m){
+    if(memo[m-1] != -1){
+        return memo[m-1];
+    }
+    if(m == 1){
+        return 1;
+    }
+    if(m == 2){
+        return 2;
+    }
+    memo[m-1] = (int)((long long)(tiling(m-1)+tiling(m-2))%1000000007);
+    return memo[m-1];
+}
