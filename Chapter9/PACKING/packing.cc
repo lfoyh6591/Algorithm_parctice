09c0c30ae9d38720b870744e81893d64d3047423
lfoyh6591 <lfoyh6591@naver.com>
2021-07-18 21:54:38 +0900

    unusing recursion while reconstruct

---
+++ Chapter9/PACKING/packing.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int memo[1001][101];
+int n, bagw;
+int want[100], weight[100];
+string name[100];
+vector<string> items;
+void unpacking();
+int packing(int, int);
+int main(){
+    int C;
+    cin >> C;    
+    for(int c=0; c<C; c++){
+        memset(memo, -1, sizeof(memo));
+        cin >> n;
+        cin >> bagw;
+        for(int i=0; i<n; i++){
+            string s;
+            cin >> s;
+            name[i] = s;
+            int x;
+            cin >> x;
+            weight[i] = x;
+            cin >> x;
+            want[i] = x;
+        }
+        int result = packing(bagw, 0);
+        unpacking();
+        cout << packing(bagw, 0) << " " << items.size() << endl;
+        int x = items.size();
+        for(int i=0; i<x; i++){
+            string s = items.back();
+            items.pop_back();
+            cout << s << endl;
+        }
+        
+    }
+}
+
+int packing(int w, int item){
+    if(memo[w][item] != -1){
+        return memo[w][item];
+    }
+    if(item == n){
+        memo[w][n] == 0;
+        return 0;
+    }
+    int& ret = memo[w][item];
+    ret = packing(w, item+1);
+    
+    if(w>=weight[item]){
+        ret = max(ret, packing(w-weight[item], item+1) + want[item]);
+    }
+    return ret;
+}
+
+
+void unpacking(){
+    int k = bagw;
+    for(int i=0; i<n; i++){
+        if(k-weight[i]>=0){
+            if(packing(k,i) == packing(k-weight[i],i+1) + want[i]){
+                items.push_back(name[i]);
+                k -= weight[i];
+            }
+        }
+    }
+}
