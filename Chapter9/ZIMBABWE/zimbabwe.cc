eb2748244d13fe41bd3c4203b54b6421ed1fd388
lfoyh6591 <lfoyh6591@naver.com>
2021-08-01 09:56:54 +0900

    changes argument

--- Chapter9/ZIMBABWE/zimbabwe.cc
+++ Chapter9/ZIMBABWE/zimbabwe.cc
@@ -10,11 +10,13 @@ int check[10];
 int num1[15];
 int n;
 int result;
-void makenum(int[10], long long ,int, bool);
+int memo[100];
+void makenum(long long ,int, bool);
 int main(){
     int C;
     cin >> C;
     for(int c=0; c<C; c++){
+        memset(memo, -1, sizeof(memo));
         result = 0;
         cin >> e;
         cin >> m;
@@ -28,7 +30,7 @@ int main(){
             check[e1%10]++;
             e1/=10;
         }
-        makenum(check, 0, 0, true);
+        makenum(0, 0, true);
         if(e%m==0){
             result--;
         }
@@ -36,7 +38,7 @@ int main(){
     }
 }
 
-void makenum(int check1[10], long long num, int length, bool issame){
+void makenum(long long num, int length, bool issame){
     if(length == n){
         if(num%m == 0){
             result++;
@@ -46,15 +48,15 @@ void makenum(int check1[10], long long num, int length, bool issame){
     }
     if(issame){
         for(int i=0; i<=num1[n-length-1]; i++){
-            if(check1[i] != 0){
+            if(check[i] != 0){
                 if(i == num1[n-length-1]){
                     check[i]--;
-                    makenum(check1, num*10+i, length+1, true);
+                    makenum(num*10+i, length+1, true);
                     check[i]++;
                 }
                 else{
                     check[i]--;
-                    makenum(check1, num*10+i, length+1, false);
+                    makenum(num*10+i, length+1, false);
                     check[i]++;
                 }
             }
@@ -62,10 +64,10 @@ void makenum(int check1[10], long long num, int length, bool issame){
     }
     else{
         for(int i=0; i<10; i++){
-            if(check1[i] != 0){
-                check1[i]--;
-                makenum(check1, num*10+i, length+1, false);
-                check1[i]++;
+            if(check[i] != 0){
+                check[i]--;
+                makenum(num*10+i, length+1, false);
+                check[i]++;
             }
         }
     }
