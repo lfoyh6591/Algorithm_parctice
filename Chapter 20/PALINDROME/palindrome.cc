724683680d777f4147b0fc3c797fbb86253c3dd4
lfoyh6591 <lfoyh6591@naver.com>
2021-09-10 19:18:33 +0900

    solve PALINDROME

--- Chapter 20/PALINDROME/palindrome.cc
+++ Chapter 20/PALINDROME/palindrome.cc
@@ -1,19 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int palindrome_length(string s){
+vector<int> getPartialMatch(string s){
     int len = s.size();
-    for(int i=0; i<len; i++){
-        if(s[i] == s[len-1]){
-            bool updated = true;
-            for(int j=0; j<=(len-i)/2; j++){
-                if(s[i+j]!=s[len-1-j]){
-                    updated = false;
-                    break;
-                }
+    vector<int> v(len, 0);
+
+    int begin = 1, matched = 0;
+    while(begin+matched<len){
+        if(s[begin+matched] == s[matched]){
+            matched++;
+            v[begin+matched-1] = matched;
+        }
+        else{
+            if(matched==0){
+                begin++;
             }
-            if(updated){
-                return len+i;
+            else{
+                begin+=matched-v[matched-1];
+                matched = v[matched-1];
+            }
+        }
+    }
+    return v;
+}
+
+int palindrome_length(string reverses){
+    string s = reverses;
+    reverse(reverses.begin(), reverses.end());
+    int len = s.size();
+
+    vector<int> v = getPartialMatch(reverses);
+
+    int begin = 0;
+    int matched = 0;
+    while(begin < len){
+        if(matched < len && s[begin+matched] == reverses[matched]){
+            matched++;
+            if(begin+matched == len){
+                return len+begin;
+            }
+        }
+        else{
+            if(matched==0){
+                begin++;
+            }
+            else{
+                begin += matched - v[matched-1];
+                matched = v[matched-1];
             }
         }
     }
