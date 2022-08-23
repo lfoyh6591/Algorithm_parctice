3a7af0192687b51d5a40bf71b273030897e8e833
lfoyh6591 <lfoyh6591@naver.com>
2022-08-23 17:21:14 +0900

    solving 1845 using heapq

---
+++ leetcode/Problem1845/solution2.py
@@ -0,0 +1,15 @@
+class SeatManager:
+
+    def __init__(self, n: int):
+        self.seat = [i+1 for i in range(n)]
+            
+    def reserve(self) -> int:
+        return heapq.heappop(self.seat)
+    
+    def unreserve(self, seatNumber: int) -> None:
+        heapq.heappush(self.seat, seatNumber)
+
+# Your SeatManager object will be instantiated and called as such:
+# obj = SeatManager(n)
+# param_1 = obj.reserve()
+# obj.unreserve(seatNumber)
